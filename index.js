7972e19a865b6c86b0eff9828b8ccc643f5f1954
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-10-16 02:50:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -61,4 +61,5 @@ function placeOrder(cardNumber) {
   
   var total = total();
   cart.splice(0, cart.length)
+  return `Your total cost is ${total}, which will be charged to the card ${cardNumber}.`;
 }
