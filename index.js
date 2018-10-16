d1bad024b674af3abd30a35fea3558910f7d0e98
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-10-16 02:56:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
-  if(cardNumber === ""){
+  if(cardNumber === undefined){
     return "Sorry, we don't have a credit card on file for you."
   }
   
