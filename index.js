1f9f26e9c45c959f1124adb058353bbbb7679582
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-10-16 02:53:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   }
   
-  var total = total();
+  var amt = total();
   cart.splice(0, cart.length)
-  return `Your total cost is ${total}, which will be charged to the card ${cardNumber}.`;
+  return `Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`;
 }
