a413eb1343a9b871abb550f1372d7418ad285951
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-10-16 02:36:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,10 +11,26 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var price = Math.floor(Math.random() * 100)
+ item.push({ itemName: name, itemPrice: price})
+ return `${item} has been added to your cart.`
 }
 
 function viewCart() {
   // write your code here
+  var string = "In your cart, you have "
+  var length = cart.length;
+  if(length === 0){
+    return "Your shopping cart is empty."
+  }
+  for(var i = 0; i < length; i++){
+    if(i == length - 1 && i !== 0){
+      string += "and "
+    }
+    string += `${cart[i].itemName} + at $${cart[i].itemPrice}`;
+    punctuation = (i == length - 1) ? "." : ",";
+    string += punctuation
+  }
 }
 
 function total() {
